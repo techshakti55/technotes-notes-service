7d81728f607a3994595b0b21957ddcbfc1d6bbda
techshakti55 <techshakti55@gmail.com>
2026-09-02 12:09:44 +0530

    feat: TECH-11 initialize notes service baseline

---
+++ src/main/java/com/technotes/notes/TechnotesNotesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.technotes.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnotesNotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnotesNotesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technotes/notes/TechnotesNotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technotes.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnotesNotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
